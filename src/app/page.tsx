e0b95a8ca8f2177dbf705d7b60125abc3d943559
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-04-03 00:36:48 +0300

    Update page layout and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from 'next/image'
 export default function Home() {
   return (
-    <main className="w-full h-[100vh]">
+    <main className="w-full mn-h-[100vh]">
       <section className="h-[100vh] w-full flex justify-center items-center">
         <video
           autoPlay
@@ -39,73 +39,109 @@ export default function Home() {
         <p className="text-white text-center">hello</p>
       </section>
 
-      <section
-        className="p-5 sm:p-5 md:p-20 lg:p-40 w-full bg-white flex flex-col gap-10"
-        style={{
-          backgroundImage: `url(/images/bg.png)`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="w-full flex flex-col justify-center items-center gap-5">
-          <h2 className="text-amber-700">OUR MISSION</h2>
-          <h1 className="md:text-2xl lg:text-5xl text-center text-black">
-            To be a force for positive change in the beauty industry, advocating
-            for inclusivity, diversity, and self-acceptance, while providing
-            services that celebrate individual beauty in all its forms.
-          </h1>
-        </div>
-        <hr className="text-black" />
-        <div className="w-full flex lg:flex-row flex-col gap-10 l">
-          <div className="lg:w-[50%] w-full"></div>
-          <div className="lg:w-[50%] w-full flex flex-col justify-center gap-5">
-            <h2 className="text-amber-700 lg:text-left text-center">ABOUT US</h2>
-            <h1 className="text-2xl md:text-5xl lg:text-left text-center text-black">
-              Where science and beauty collide
-            </h1>
-            <p className="text-xl text-black lg:text-left text-center">
-              Step into our world and experience the power of transformation.
-              Unleash your confidence and embrace your ageless beauty with us.
-            </p>
-            <div className="flex gap-5">
-<Image
-src='/images/aroma.png'
-alt="bg"
-width={32}
-height={32}
-/>
-<div>
-<h1 className="text-xl md:text-2xl lg:text-left text-center text-black">
-Advanced laser systems
-            </h1>
-            <p className="text-l text-black lg:text-left text-center">
-            Discover the benefits of our modern cosmetology and dermatology equipment, carefully selected to provide you with the best experience.
-            </p>
-</div>
-            </div>
-            <div className="flex gap-5">
-<Image
-src='/images/aroma.png'
-alt="bg"
-width={32}
-height={32}
-/>
-<div>
-<h1 className="text-xl md:text-2xl lg:text-left text-center text-black">
-Advanced laser systems
-            </h1>
-            <p className="text-l text-black lg:text-left text-center">
-            Discover the benefits of our modern cosmetology and dermatology equipment, carefully selected to provide you with the best experience.
-            </p>
-</div>
-            </div>
-          </div>
-          
-        </div>
-      </section>
+      <section className="p-10 w-full bg-[#c96c4e]">
+  <p className="text-white text-center">hello</p>
+</section>
 
-      <section className="h-[100vh] w-full"></section>
-      <section className="h-[100vh] w-full"></section>
+<section
+  className="p-5 sm:p-5 md:p-20 lg:p-40 w-full bg-white flex flex-col gap-10"
+  style={{
+    backgroundImage: "url(/images/bg.png)",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  }}
+>
+  <div className="w-full flex flex-col justify-center items-center gap-5">
+    <h2 className="text-amber-700">OUR MISSION</h2>
+    <h1 className="md:text-2xl lg:text-5xl text-center text-black">
+      To be a force for positive change in the beauty industry, advocating for
+      inclusivity, diversity, and self-acceptance, while providing services that
+      celebrate individual beauty in all its forms.
+    </h1>
+  </div>
+  <hr className="text-black" />
+  <div className="w-full flex lg:flex-row flex-col md:gap-10 gap-30">
+  <div className="lg:w-[50%] w-full relative flex justify-center items-center h-full">
+  <Image
+    src="/images/y5h35yh.png"
+    alt="Background"
+    width={450} // Adjusted size for the first image
+    height={450}
+   // Slightly enlarges the background image
+  />
+  <div className="absolute inset-0 flex justify-center items-center">
+    <Image
+      src="/images/3y5h5yhy5f.png"
+      alt="Foreground"
+      width={350} // Adjusted size for the second image
+      height={350}
+    />
+  </div>
+</div>
+    <div className="lg:w-[50%] w-full flex flex-col justify-center gap-6">
+      <div className=" flex flex-col gap-2">
+      <h2 className="text-amber-700 lg:text-left text-center">ABOUT US</h2>
+      <h1 className="text-2xl md:text-6xl lg:text-left text-center text-black">
+        Where science and beauty collide
+      </h1>
+      </div>
+   
+      <p className="text-xl text-black lg:text-left text-center">
+        Step into our world and experience the power of transformation. Unleash
+        your confidence and embrace your ageless beauty with us.
+      </p>
+      <div className="flex gap-5">
+        <Image src="/images/aroma.png" alt="bg" width={32} height={32} />
+        <div>
+          <h1 className="text-xl md:text-2xl lg:text-left text-center text-black">
+            Advanced laser systems
+          </h1>
+          <p className="text-l text-black lg:text-left text-center">
+            Discover the benefits of our modern cosmetology and dermatology
+            equipment, carefully selected to provide you with the best
+            experience.
+          </p>
+        </div>
+      </div>
+      <div className="flex gap-5">
+        <Image src="/images/aroma.png" alt="bg" width={32} height={32} />
+        <div>
+          <h1 className="text-xl md:text-2xl lg:text-left text-center text-black">
+            Advanced laser systems
+          </h1>
+          <p className="text-l text-black lg:text-left text-center">
+            Discover the benefits of our modern cosmetology and dermatology
+            equipment, carefully selected to provide you with the best
+            experience.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section
+  className="h-[50vh] w-full flex flex-col justify-center items-center bg-black bg-opacity-80 gap-5 py-10"
+  style={{
+    backgroundImage: "url(/images/yh5h5yhb-min.png)",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  }}
+>
+<h1 className="text-white text-5xl md:text-8xl text-center">
+Your skin, your confidence.
+
+          </h1>
+          <p className="md:text-3xl text-xl text-white">
+            Find the right skincare services with our specialists.
+          </p>
+            <Link href="/" className="px-10 py-4 bg-amber-600">
+              BOOK NOW
+            </Link>
+      </section>
+      <section className="h-[100vh] w-full">
+      
+      </section>
     </main>
   );
 }
