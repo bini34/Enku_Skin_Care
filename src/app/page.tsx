319b2bd038128537757ab769a22f389780c43a9c
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-04-02 13:21:17 +0300

    Update video background and content layout on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
         loop
         muted
         playsInline
-        className="absolute top-0 left-0 w-full  object-cover -z-10 "
+        className="absolute top-0 left-0 w-full h-full  object-cover -z-10 "
       >
         <source src="videos/bg.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
- <div className="md:w-[70%] w-full flex flex-col items-center align-middle gap-10 mb-28 ">
-  <h1 className="text-white  text-5xl md:text-9xl text-center">Experience the art of skincare</h1>
-  <p className="text-2xl">Find the right skincare services with our specialists.</p>
-  <div className="w-full px-10 flex gap-5 justify-center">
+ <div className="md:w-[70%] w-[90%] flex flex-col items-center align-middle gap-10 mb-28 ">
+  <h1 className="text-white  text-7xl md:text-9xl text-center">Experience the art of skincare</h1>
+  <p className="md:text-3xl text-xl">Find the right skincare services with our specialists.</p>
+  <div className="flex md:flex-row flex-col gap-5 justify-center">
   <Link href="/" className='px-10 py-5 bg-amber-600'>BOOK NOW</Link>
   <Link href="/" className='px-10 py-5 border-amber-600 border-2 border-solid hover:bg-amber-500 '>LEARN MORE</Link>
   </div>
