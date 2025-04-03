e5cc7a76a288106def3031eaac4308f6ac0fd24f
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-04-03 04:14:00 +0300

    Update global CSS styles and dark mode support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import Image from 'next/image'
+import {StarFilledIcon } from "@radix-ui/react-icons"
+
 export default function Home() {
   return (
     <main className="w-full mn-h-[100vh]">
-      <section className="h-[100vh] w-full flex justify-center items-center">
+      <section className="h-[100vh] w-full flex justify-center items-center text-white">
         <video
         
           autoPlay
@@ -16,14 +18,14 @@ export default function Home() {
           Your browser does not support the video tag.
         </video>
         <div className="md:w-[70%] w-[90%] flex flex-col items-center align-middle gap-10 mb-28">
-          <h1 className="text-white text-7xl md:text-9xl text-center">
+          <h1 className=" text-7xl md:text-9xl text-center">
             Experience the art of skincare
           </h1>
           <p className="md:text-3xl text-xl">
             Find the right skincare services with our specialists.
           </p>
           <div className="flex md:flex-row flex-col gap-5 justify-center">
-            <Link href="/" className="px-10 py-4 bg-amber-600">
+            <Link href="/" className="px-10 py-4 bg-amber-600 text[#2f2b2a]">
               BOOK NOW
             </Link>
             <Link
@@ -39,10 +41,6 @@ export default function Home() {
       <section className="p-10 w-full bg-[#c96c4e]">
         <p className="text-white text-center">hello</p>
       </section>
-
-      <section className="p-10 w-full bg-[#c96c4e]">
-  <p className="text-white text-center">hello</p>
-</section>
 
 <section
   className="p-5 sm:p-5 md:p-20 lg:p-40 w-full bg-white flex flex-col gap-10"
@@ -136,7 +134,7 @@ Your skin, your confidence.
           <p className="md:text-3xl text-xl text-white">
             Find the right skincare services with our specialists.
           </p>
-            <Link href="/" className="px-10 py-4 bg-amber-600">
+            <Link href="/" className="px-10 py-4 bg-amber-600 text-white">
               BOOK NOW
             </Link>
       </section>
@@ -224,6 +222,70 @@ DISCOVER
     </div>
   </div>
       
+      </section>
+      <section className="w-full m-h-[70vh] bg-[#c96c4e] flex lg:flex-row flex-col gap-2 justify-center items-center sm:p-15 p-5">
+        <div className="sm:w-[500px]   w-full flex flex-col justify-center  items-center text-center bg-[#FFFFFFB3] p-10 gap-5">
+            <div className="flex  gap-2 text-2xl text-[#c96c4e]">
+            <StarFilledIcon className=""/> <StarFilledIcon/> <StarFilledIcon/> <StarFilledIcon/> <StarFilledIcon/>
+           </div>
+           <p className="text-xl text-[#2f2b2a]">
+           The cosmetologists are highly skilled and knowledgeable, providing personalized recommendations and delivering outstanding results. I left feeling rejuvenated and confident.
+
+          </p>
+          <h2 className="text-lg text-[#2f2b2a]">By <span className="text-2xl">Sarah Thompson</span></h2>
+          <p className="text-[#c96c4e]">– 1 month ago –</p>
+        </div>
+        <div className="sm:w-[500px] w-full flex flex-col justify-center  items-center text-center bg-[#FFFFFFB3] p-10 gap-5">
+            <div className="flex  gap-2 text-2xl text-[#c96c4e]">
+            <StarFilledIcon className=""/> <StarFilledIcon/> <StarFilledIcon/> <StarFilledIcon/> <StarFilledIcon/>
+           </div>
+           <p className="text-xl text-[#2f2b2a]">
+           The cosmetologists are highly skilled and knowledgeable, providing personalized recommendations and delivering outstanding results. I left feeling rejuvenated and confident.
+
+          </p>
+          <h2 className="text-lg text-[#2f2b2a]">By <span className="text-2xl">Sarah Thompson</span></h2>
+          <p className="text-[#c96c4e]">– 1 month ago –</p>
+        </div>
+        <div className="sm:w-[500px] w-full flex flex-col justify-center  items-center text-center bg-[#FFFFFFB3] p-10 gap-5">
+            <div className="flex  gap-2 text-2xl text-[#c96c4e]">
+            <StarFilledIcon className=""/> <StarFilledIcon/> <StarFilledIcon/> <StarFilledIcon/> <StarFilledIcon/>
+           </div>
+           <p className="text-xl text-[#2f2b2a]">
+           The cosmetologists are highly skilled and knowledgeable, providing personalized recommendations and delivering outstanding results. I left feeling rejuvenated and confident.
+
+          </p>
+          <h2 className="text-lg text-[#2f2b2a]">By <span className="text-2xl">Sarah Thompson</span></h2>
+          <p className="text-[#c96c4e]">– 1 month ago –</p>
+        </div>
+
+      </section>
+      <section className="w-full m-h-[70vh] flex gap-10  lg:flex-row flex-col justify-center items-center lg:p-2 md:p-20 p-5"
+       style={{
+        background: "linear-gradient(180deg, #DFC1A3 0%, #FEEFDF 100%)",
+      }}>
+         <div className="lg:w-[50%] w-full flex  justify-center items-center">
+         <Image
+      src="/images/skjrnhrs.png"
+      alt="Foreground"
+      width={400} // Adjusted size for the second image
+      height={250}
+    />
+         </div>
+         <div className="lg:w-[50%] w-full flex flex-col justify-center items-center lg:items-start  gap-5">
+         <h1 className=" text-4xl md:text-6xl lg:text-8xl text-center lg:text-left ">
+         Enhance your natural beauty
+
+
+          </h1>
+          <p className=" text-xl lg:text-left text-center">
+          Step into our world and experience the power of transformation. Unleash your confidence and embrace your ageless beauty with us.
+
+          </p>
+            <Link href="/" className="px-10 py-4 bg-amber-600 text-white">
+              BOOK NOW
+            </Link>
+
+</div>
       </section>
     </main>
   );
