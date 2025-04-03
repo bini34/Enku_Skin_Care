148f6247589972695a6e33a3f3d089a076bb9729
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-04-03 04:14:08 +0300

    Update footer styling and copyright information

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='w-full bg-black'>
-      footer
+    <footer className='w-full bg-amber-50 flex items-center justify-center'>
+      <h1 >
+      Copyright © 2025 Enku. All rights reserved.
+
+      </h1>
     </footer>
   )
 }
