eaaa3126b1b107729cc80f591d98c22d18eb2744
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-04-06 14:12:39 +0300

    Refactor layout component and add Toaster component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import {  Marcellus } from "next/font/google";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
-import "./globals.css";
-
+import type { Metadata } from 'next';
+import { Marcellus } from 'next/font/google';
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+import './globals.css';
+import { Toaster } from "@/components/ui/sonner"
 
 
 const marcellus = Marcellus({
-  variable: "--font-marcellus",
-  subsets: ["latin"],
-  weight: "400", // Marcellus has only one weight
+  variable: '--font-marcellus',
+  subsets: ['latin'],
+  weight: '400', // Marcellus has only one weight
 });
 
 export const metadata: Metadata = {
-  title: "Enku Skin Care",
-  description: "",
+  title: 'Enku Skin Care',
+  description: '',
 };
 
 export default function RootLayout({
@@ -24,12 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={` ${marcellus.variable} font-marcellus antialiased bg-amber-50 text-black`}
-      >
+      <body className={` ${marcellus.variable} font-marcellus antialiased bg-amber-50 text-black`}>
         <Header />
         {children}
         <Footer />
+        <Toaster />
       </body>
     </html>
   );
