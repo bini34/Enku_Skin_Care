142a87a57a4243c91e7cd6b08b8a97469788fa4c
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-04-06 14:11:48 +0300

    Refactor reservation page component

--- src/app/Reservation/page.tsx
+++ src/app/Reservation/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function page() {
-  return (
-    <main>
-      reservation
-    </main>
-  )
+  return <main>reservation</main>;
 }
 
-export default page
+export default page;
