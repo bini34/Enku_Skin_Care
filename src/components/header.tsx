60a07d07251861df08819c84bf35ec4d71ab5b49
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-04-06 14:11:17 +0300

    Refactor header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Nav from './navbar'
-import Link from 'next/link'
+import React from 'react';
+import Nav from './navbar';
+import Link from 'next/link';
 function Header() {
   return (
-    <header className='flex  align-middle justify-between px-10 py-10 bg-transparent text-white '>
-        <h1 className='align-middle'>Enku</h1>
-        <Nav/>
-        <Link href="/" className='px-5 py-2 bg-amber-600 md:block hidden'>BOOK NOW</Link>
+    <header className="flex  align-middle justify-between px-10 py-10 bg-transparent text-white ">
+      <h1 className="align-middle">Enku</h1>
+      <Nav />
+      <Link href="/" className="px-5 py-2 bg-amber-600 md:block hidden">
+        BOOK NOW
+      </Link>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
